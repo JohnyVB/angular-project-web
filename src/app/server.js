b7bb670a04fa1929293d041f8ea8c78148c692d2
Johny Villegas <55726826+JohnyVB@users.noreply.github.com>
2020-09-23 12:59:48 -0500

    Se removio el servicio de express y path

--- src/app/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
-
-app.use(express.static(__dirname + '/src'));
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/src/index.html'));
-});
